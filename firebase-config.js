37aa44e84e6d241a72f0103450e3efcda35cc060
reissondesigner-lang <reissondesigner@gmail.com>
2026-02-16 16:45:45 -0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBbX57rW9ws4j28LxIVr4KM1CGxqghm4_8",
+  authDomain: "nipponrefis.firebaseapp.com",
+  projectId: "nipponrefis",
+  storageBucket: "nipponrefis.firebasestorage.app",
+  messagingSenderId: "714981606481",
+  appId: "1:714981606481:web:be99b6ebd430231c212470"
+};
