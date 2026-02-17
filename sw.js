c1dc0c86c23b1f57dbf15d27dc3679d304b50d3c
reissondesigner-lang <reissondesigner@gmail.com>
2026-02-17 01:24:17 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = 'nipponsync-v1';
+const assets = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
