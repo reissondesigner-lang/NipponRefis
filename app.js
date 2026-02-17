2e60c31f33535e01a6fae92a074121adf01bc6be
reissondesigner-lang <reissondesigner@gmail.com>
2026-02-17 15:17:15 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
 import { getFirestore, collection, addDoc, query, where, getDocs, doc, updateDoc, getDoc, setDoc, orderBy } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-firestore.js";
-import { firebaseConfig } from "./firebase-config.js";
+import { auth, db } from "./firebase-config.js";
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
+
 
 let clienteEmEdicaoId = null; // Começa como null (significa novo cadastro)
 let usuarioLogado = null;
